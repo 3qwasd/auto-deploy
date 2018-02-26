acf625e2d255d16006cfb948e55516f2faa90639
root <qiaojian@hooyue.comgiggit>
2018-02-26 10:07:30 +0800

    2018/02/26 demo1

---
+++ demo-1/src/main/java/com/demo/Demo1Application.java
@@ -0,0 +1,23 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+	
+	@GetMapping("hello")
+	@ResponseBody
+	public String helloWord() {
+		
+		return "Hello World!";
+	}
+}
